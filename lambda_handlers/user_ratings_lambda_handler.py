64957163b0ec985cbb893262ec9a8ec57b87f62d
Cole Duclos <cole.duclos@slalom.com>
2016-12-19 12:51:13 -0800

    integrated lambda CORS

--- lambda_handlers/user_ratings_lambda_handler.py
+++ lambda_handlers/user_ratings_lambda_handler.py
@@ -15,11 +15,12 @@ def respond(err, res=None):
         'body': err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
     }
 
 def lambda_handler(event, context):
-
+    print ('Lambda Event Handler: duclos-app-rating')
     operations = {
         'DELETE': lambda dynamo, x: dynamo.delete_item(Key=
             {
@@ -30,20 +31,24 @@ def lambda_handler(event, context):
         'POST': lambda dynamo, x: dynamo.put_item(Item=x),
         'PUT': lambda dynamo, x: dynamo.update_item(Item=x),
     }
-    
+
     operation = event['httpMethod']
-    
+
     if operation in operations:
-        
+        print event['body']
         payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
         dynamo = boto3.resource('dynamodb').Table(user_ratings_dynamo_table_name)
-        operations[operation](dynamo, payload)
-        
+        rating = {}
+        rating['user-id'] = payload['user-id']
+        rating['restaurant-id'] = payload['restaurant']['restaurant-id']
+        rating['rating-value'] = payload['rating-value']
+        operations[operation](dynamo, rating)
+
         sqs = boto3.resource('sqs')
         queue = sqs.get_queue_by_name(QueueName=queue_name)
         response = queue.send_message(MessageBody=json.dumps(event))
-        
+
         return respond(None, response)
-        
+
     else:
         return respond(ValueError('Unsupported method "{}"'.format(operation)))
